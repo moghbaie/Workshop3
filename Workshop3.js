bc9b1efb6f8aa169340aa7b259fbbe62cff613fd
moghbaie <moghbaie@users.noreply.github.com>
2015-08-09 16:32:24 -0400

    Update Workshop3.js

--- Workshop3.js
+++ Workshop3.js
@@ -2,31 +2,28 @@ var myApp=angular.module('test01', ['ServicesModule']);
 
 
 myApp.controller('MyController',function($scope,userpref){
-	var self=this;
+  var self=this;
 
 
-	self.pref=new userpref([]);
+  self.pref=new userpref(self.name, self.age, self.type, self.songcheck);
 
-	self.songlist={
+  self.songlist={
   Pop:[{SongName:'Black Space'},{SongName:'Mashup'}],
   Rock:[{SongName:'The Wolf'},{SongName:'Black Sun'},{SongName:'Delilah'},{SongName:'Psycho'}],
   Rap:[{SongName:'Blessings'},{SongName:'Ride Out'},{SongName:'Be Real'},{SongName:'R.I.C.O.'}]
   };
 
   self.genrechange=function(choice){
-   		self.selectedlist=self.songlist[choice];
+      self.selectedlist=self.songlist[choice];
    };
+
+  self.songcheck=[];
 
   self.makelist=function(SongName,active){
       if(active){
-            self.song.list.push(SongName);
+            self.songcheck.push(SongName);
         }else{
-             self.song.list.splice(self.song.list.indexOf(SongName), 1);
+             self.songcheck.splice(self.songcheck.indexOf(SongName), 1);
         }      
    };
-
-	self.addPref = function(song) {
-		self.pref.song.addSong(song);
-    
-	};
 });
